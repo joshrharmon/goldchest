6957bddc82be58c3ad93a1e2ba5f2de214ef53e1
jakerosen <jakesrosen@gmail.com>
2021-03-22 16:43:55 -0700

    fixed requirements

--- django-rest-react-prototype/frontend/src/components/BestDealsGrid.js
+++ django-rest-react-prototype/frontend/src/components/BestDealsGrid.js
@@ -1,70 +1,55 @@
-
-
-
 import React, {Component} from "react";
 
-export class BestDealsGrid extends Component
-{
+export class BestDealsGrid extends Component {
+  html(deal){
+    return (
 
+      <div className="col-md-4 product-grid">
 
-    html(deal){
-        return (
-
-            <div className="col-md-4 product-grid">
-            
-            <div className="image">
-                <a href="#">                              
-                <div className="overlay">
-                    <div className="detail">View Details</div>
-                </div>
-                </a>
+        <div className="image">
+          <a href="#">
+            <div className="overlay">
+              <div className="detail">View Details</div>
             </div>
-            <h5 className="text-center">{deal.title}</h5>
-            <h5 className="text-center">{deal.price_low}</h5>
-            <a href={deal.url} className="btn buy">VIEW DETAILS</a>
-            
+          </a>
         </div>
-        );
-    }
-//<img src="images/sparklingwater.jpg" alt="description of picture" className="w-100"/>
+        <h5 className="text-center">{deal.title}</h5>
+        <h5 className="text-center">{deal.price_low}</h5>
+        <a href={deal.url} className="btn buy">VIEW DETAILS</a>
+
+      </div>
+    );
+  }
+  //<img src="images/sparklingwater.jpg" alt="description of picture" className="w-100"/>
 
 
-    render()
-    {
-        console.warn(this.props.bestDealsList);
-        const mapHTMLtoNrOfDeals = this.props.bestDealsList.map( deal => {
+  render() {
+    console.warn(this.props.bestDealsList);
+    const mapHTMLtoNrOfDeals = this.props.bestDealsList.map( deal => {
+      return this.html(deal);
+    });
 
-            return this.html(deal);
+    return(
 
-        } );
+      <div>
 
-        return(
-
-            <div>
-
-
-                <div className="container-fluid padding">
-                    <div className="row welcome text-center">
-                        <div className="col-12">
-                            <h1 className="display-4"> .</h1>
-                        </div>
-
-                        <div className="col-12">
-                            <p className="lead">See our hottest deals right now!</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-
-
-                    {mapHTMLtoNrOfDeals}
-
-
-
-                </div>
-
+        <div className="container-fluid padding">
+          <div className="row welcome text-center">
+            <div className="col-12">
+              <h1 className="display-4"> .</h1>
             </div>
-        )
-    }
+
+            <div className="col-12">
+              <p className="lead">See our hottest deals right now!</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          {mapHTMLtoNrOfDeals}
+        </div>
+
+      </div>
+    )
+  }
 }
