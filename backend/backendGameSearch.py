c29b214e029b90b8dd38174cc2292a0c2f77910d
Joshua <54410912+joshrharmon@users.noreply.github.com>
2021-04-23 07:41:58 +0000

    Fully functional JSON search!
    Searching now has a front-end endpoint that returns the formal title, price and a direct link to the Steam page that returns results in a nice JSON format.

--- backend/backendGameSearch.py
+++ backend/backendGameSearch.py
@@ -3,14 +3,22 @@ import requests, json, backendGameFetch as fetch
 backFetch = fetch.gameFetch(fetch.API_KEY, fetch.DB_PATH)
 
 class gameSearch():
-	def __init__(self, api_key, db_path):
-		self.api_key = api_key
-		self.db_path = db_path
-		
-	def search(self, name, numResults, apikey):
-		apiRequest = "https://api.isthereanydeal.com/v02/search/search/?key=" + str(fetch.API_KEY) + "&q=" + backFetch.gameList(str(name)) + "&limit=" + numResults
-		print(apiRequest)
-		return backFetch.apiStatus(requests.get(apiRequest))
+	def search(self, query, numResults):
+		gamesFetched = 0
+		searchURL = "https://store.steampowered.com/search/?term='{}'".format(query)
+		soup = fetch.BeautifulSoup(requests.get(searchURL).content, features='html.parser')
+		searchArr = []
+		gameData = soup.find_all("a", class_="search_result_row ds_collapse_flag")
+		gameTitles = soup.find_all("span", class_="title")
+		gamePrices = soup.find_all("div", class_="col search_price responsive_secondrow")
+		while gamesFetched < numResults:
+			searchItem = dict()
+			searchItem["title"] = gameTitles[gamesFetched].contents[0]
+			searchItem["price"] = backFetch.priceFormat(gamePrices[gamesFetched].contents[0])
+			searchItem["url"] = gameData[gamesFetched]['href']
+			searchArr.append(searchItem)
+			gamesFetched += 1
+		return searchArr
 		
 	def start(self):
 		fetch.api.add_resource(Search, '/search')
@@ -27,5 +35,4 @@ class Search(fetch.Resource):
 		parser.add_argument('query', required=True)
 		parser.add_argument('limit', required=True)
 		args = parser.parse_args()
-		print(args['query'], args['limit'], fetch.API_KEY)
-		return json.loads(json.dumps(gameSearch(fetch.API_KEY, fetch.DB_PATH).search(args['query'], args['limit'], fetch.API_KEY))), 200
+		return gameSearch().search(args['query'], int(args['limit'])), 200
