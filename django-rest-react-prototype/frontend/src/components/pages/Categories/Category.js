3c8215b37cb86560ed422cc7659c5a4c2b9da49f
johannafransn <adrina0002@gmail.com>
2021-05-03 09:12:53 -0700

    fixed category endless loop again

--- django-rest-react-prototype/frontend/src/components/pages/Categories/Category.js
+++ django-rest-react-prototype/frontend/src/components/pages/Categories/Category.js
@@ -22,52 +22,71 @@ export class Category extends Component {
 
 
     state = {
+        load: false,
         games: []
     }
 
 
+    fetchApiData(){
+        //get an object with the link to which url linker we are on
+        const categoryID = this.props.match.params
+        //get the first element in the categoryID object
+        const firstElementInCategoryID = Object.values(categoryID)[0]
+        console.log(firstElementInCategoryID)
+        console.log('Do something with it', categoryID);
+        //console.log('http://localhost:5000/deals?cat=' + firstElementInCategoryID + '&num=12')
+        fetch('http://localhost:5000/deals?cat=' + firstElementInCategoryID + '&num=6')
+            .then((response) => response.json())
+            .then(gamesList => {
+                this.setState({
+                    games: gamesList
+                    });
+                console.log(this.games)
+            });
+    }
+
     componentDidMount() {
-            //get an object with the link to which url linker we are on
-            const categoryID = this.props.match.params
-            //get the first element in the categoryID object
-            const firstElementInCategoryID = Object.values(categoryID)[0]
-            console.log(firstElementInCategoryID)
-            console.log('Do something with it', categoryID);
-            //console.log('http://localhost:5000/deals?cat=' + firstElementInCategoryID + '&num=12')
-            fetch('http://localhost:5000/deals?cat=' + firstElementInCategoryID + '&num=6')
-                .then((response) => response.json())
-                .then(gamesList => {
-                    this.setState({games: gamesList});
-                    console.log(this.games)
-                });
-
+        this.fetchApiData()
     }
 
 
-    componentDidUpdate() {
-        if (this.props.games && !this.state.games) {
-            console.log('Games state has changed.')
-            this.componentDidMount()
+    componentDidUpdate(prevProps, prevState) {
+        //get an object with the link to which url linker we are on
+        const categoryID = this.props.match.params
+        //get the first element in the categoryID object
+        const firstElementInCategoryID = Object.values(categoryID)[0]
+
+        //Debugging to see statements to see which variables to compare
+        console.log('hello' + this.state)
+        console.log('hello2' + prevState.state)
+        console.log('hello3' + prevState.state)
+        console.log('Games array' , this.state.games)
+        console.log('category content ' + this.state.games[0].category)
+        console.log('firstElementInCategoryID' + firstElementInCategoryID)
+        console.log('hello5' + categoryID)
+        console.log('hello' + this.state)
+
+        //compare current category with the category state from the games list
+        // (when you change state clicking on a different category)
+        if (firstElementInCategoryID !== this.state.games[0].category) {
+            console.log('State has changed.')
+            this.fetchApiData()
         }
-    }
+        }
 
 
 
 
-render() {
 
+render()
+{
     const categoryIDhej = this.props.match.params
     //get the first element in the categoryID object
     const catName = Object.values(categoryIDhej)[0]
-
     return(
         <div>
             <CategoryGrid/>
-
-
             <div>
-
-
                 <div className="container-fluid padding">
                     <div className="row welcome text-center">
                         <div className="col-12">
@@ -78,23 +97,16 @@ render() {
                         </div>
                     </div>
                 </div>
-
                 <div className="row">
-
             {this.state.games.map((game) => (
-
                     <div className="col-md-4 product-grid">
                         <h5 className="text-center">{game.title}</h5>
                         <img src={game.art} alt="" className="w-100" />
-
-                        <h5 className="text-center">NOW ${game.price_new} SAVE {game.price_cut}%</h5>
+                        <h5 className="text-center">NOW {game.currency}{game.price_new} SAVE {game.price_cut}%</h5>
                         <a href={game.url} className="btn buy">BUY NOW</a>
-
                     </div>
-
             ))}
         </div>
-
         </div>
         </div>
 
