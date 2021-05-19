53d2bad9f6ca114764dbddc6f49c2a1c10d5d610
HoangNguyen <hoangnguyen30196@gmail.com>
2021-05-19 10:23:31 -0700

    fixed error that cannot steam sign on

--- runServers.sh
+++ runServers.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd backend && python3 backendStart.py &
-cd django-rest-react-prototype && python3 manage.py runserver 0.0.0.0:8000
+cd django-rest-react-prototype && python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
